8044441b5c50ab87ebaaf5ef2fb44a640bbf2786
Paromita <paromitatalukder04@gmail.com>
2024-08-10 19:26:51 -0400

    Add item button and edited title

--- app/page.js
+++ app/page.js
@@ -2,25 +2,58 @@
 import Image from "next/image";
 import {useState, useEffect} from 'react'
 import {firestore} from '@/firebase'
-import {Box, Typography} from '@mui/material'
+import {Box, Typography, Modal, Stack, TextField, Button} from '@mui/material'
 import { collection, getDocs, query } from "firebase/firestore";
 
 export default function Home() {
-  const [inventory, setInventory] = useState({})
-  const [open, setOpen] = useState([])
+  const [inventory, setInventory] = useState([])
+  const [open, setOpen] = useState(false)
   const [itemName, setItemName] = useState('')
 
   const updateInventory = async () => {
     const snapshot = query(collection(firestore, 'inventory'))
-    const doc = await getDocs(snapshot)
+    const docs = await getDocs(snapshot)
     const inventoryList = []
-    DocumentSnapshot.forEach((doc) => {
-      inventory:List.push({
+
+    docs.forEach((doc) => {
+      inventoryList.push({
         name: doc.id,
         ...doc.data(),
       })
     })
+
     setInventory(inventoryList)
+  }
+
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore,'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data()
+      await setDoc(docRef, {quantity: quantity + 1})
+    }
+    else {
+      await setDoc(docRef, {quantity: 1})
+    }
+    
+      await updateInventory()
+  }
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore,'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data()
+      if(quantity === 1) {
+        await deleteDoc(docRef)
+        }
+        else {
+          await setDoc(docRef, {quantity: quantity - 1})
+        }
+    }
+      await updateInventory()
   }
 
   useEffect(() => {
@@ -28,21 +61,79 @@ export default function Home() {
   }, []
 )
 
+const handleOpen = () => setOpen(true)
+const handleClose = () => setOpen(false)
+
   return (
-    <Box>
-      <Typography variant = "h1">Inventory Management</Typography>
+    <Box 
+    width = "100vw" 
+    height = "100vh" 
+    display="flex" 
+    flexDirection = "column"
+    justifyContent="center" 
+    alignItems = "center"
+    gap = {2}>
 
-      {inventory.forEach((item) => {
-          return ( 
-          <>
-          {item.name}
-          {item.count}
-          </>
-          )
-        })}
+      <Modal open = {open} oneClose = {handleClose}>
+        <Box
+        position = "absolute"
+        top = "50%"
+        left = "50%"
+        sx = {{
+          transform: "translate(-50%, -50%)"
+        }}
+        width = {400}
+        bgcolor = "white"
+        border = "2px solid #000"
+        boxShadow = {24}
+        p = {4}
+        display= "flex"
+        flexDirection="column"
+        gap = {3}
+        >
+          <Typography variant = "h6">Add Item</Typography>
+          <Stack width = "100%" direction = "row" spacing = {2}>
+            <TextField
+            variant = "outlined"
+            fullWidth
+            value = {itemName}
+            onChange = {(e) => {
+              setItemName(e.target.value)
+            }}
+            />
+              <Button
+                  variant = "outlined" 
+                  onClick ={() => {
+                    addItem(itemName)
+                    setItemName('')
+                    handleClose()
+                }}
+                >Add</Button>
+          </Stack>
+        </Box>
+      </Modal>
 
-
-
+        <Button variant ="contained" 
+                onClick = {()=> {
+                handleOpen()}}>
+          Add New Item
+        </Button>
+        <Box border = "1px solid #333">
+          <Box
+            width = "800px"
+            height= "100px"
+            bgcolor = "#ADD8E6"
+            display = "flex"
+            alignItems = "center"
+            justifyContent = "center"
+            >
+              <Typography variant = "h2" 
+                          color = "#333"
+                          >
+                Inventory Items
+              </Typography>
+            </Box>
+        </Box>
     </Box>
   )
 }
